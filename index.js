78fd3026e2b438212f8205a7277483dfac0d71b5
andrew malcher <andrew.malcher.r@gmail.com>
2025-03-13 21:05:12 -0400

    feat: implement task retrieval from database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const TaskModel = require("./src/models/task.model");
 
 const connectToDatabase = require("./src/database/mongoose.database");
 
@@ -9,13 +10,8 @@ const app = express();
 
 connectToDatabase();
 
-app.get("/tasks", (req, res) => {
-    const tasks = [
-        {
-            description: "Terminar o gerenciador de tarefas!",
-            isCompleted: false, // Corrigir valor booleano
-        },
-    ];
+app.get("/tasks", async (req, res) => {
+    const tasks = await TaskModel.find({});
     res.status(200).send(tasks);
 });
 
